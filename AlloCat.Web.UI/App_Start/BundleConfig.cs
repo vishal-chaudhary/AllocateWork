1fa4a06482bc68cf422f160b7c8297857aa00f01
Vishal Chaudhary <vishal.chaudhary29@hotmail.com>
2017-01-04 14:03:05 +0530

    Added Angular Library and folder structure for angular implementation

--- AlloCat.Web.UI/App_Start/BundleConfig.cs
+++ AlloCat.Web.UI/App_Start/BundleConfig.cs
@@ -9,12 +9,16 @@ namespace AlloCat.Web.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/lib/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/lib/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts//lib/angular.js",
+                        "~/Scripts/lib/angular-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
@@ -22,7 +26,15 @@ namespace AlloCat.Web.UI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/jquery.css",
+                      "~/Content/pages.min.css",
+                      "~/Content/pixel-admin.min.css",
+                      "~/Content/responsive.css",
+                      "~/Content/rtl.min.css",
+                      "~/Content/style.css",
+                      "~/Content/themes.min.css",
+                      "~/Content/widgets.min.css",
+                      "~/Content/Site.css"));
         }
     }
 }
